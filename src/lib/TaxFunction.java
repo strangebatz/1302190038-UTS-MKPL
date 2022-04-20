a4ef88f0475e096a20c71460517a8f3ea6856d30
AngeliaB <angeliab66@gmail.com>
2022-04-20 17:46:16 +0700

    Mempersingkat fungsi
    Mempersingkat fungsi dari tax dengan membuat variable baru yakni totalIncome1 yang berisikan variable-variable pendapatan

--- src/lib/TaxFunction.java
+++ src/lib/TaxFunction.java
@@ -18,6 +18,7 @@ public class TaxFunction {
 	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
 		
 		int tax = 0;
+		int totalIncome1 = ((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible;
 		
 		if (numberOfMonthWorking > 12) {
 			System.err.println("More than 12 month working per year");
@@ -28,9 +29,9 @@ public class TaxFunction {
 		}
 		
 		if (isMarried) {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
+			tax = (int) Math.round(0.05 * (totalIncome1 - (54000000 + 4500000 + (numberOfChildren * 1500000))));
 		}else {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
+			tax = (int) Math.round(0.05 * (totalIncome1 - 54000000));
 		}
 		
 		if (tax < 0) {
