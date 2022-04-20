46b47b081cd7a3c88c782ac8e1093cad9eb36d14
AngeliaB <angeliab66@gmail.com>
2022-04-20 19:00:43 +0700

    mengganti value dari monthlySalary
    Mengganti value dari monthlySalary karena sebelumnya value tersebut sama semua (3000000 * 1.5)

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -56,17 +56,17 @@ public class Employee {
 		if (grade == 1) {
 			monthlySalary = 3000000;
 			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
+				monthlySalary = monthlySalary * 1.5;
 			}
 		}else if (grade == 2) {
 			monthlySalary = 5000000;
 			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
+				monthlySalary = monthlySalary * 1.5;
 			}
 		}else if (grade == 3) {
 			monthlySalary = 7000000;
 			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
+				monthlySalary = monthlySalary * 1.5;
 			}
 		}
 	}
